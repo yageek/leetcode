4069bb24edc782a970f0b3bb210dbe009cd8a59d
Yannick Heinrich <yannick.heinrich@gmail.com>
2017-04-06 09:44:02 +0200

    Fix error.

--- addTwoNumbers/main.go
+++ addTwoNumbers/main.go
@@ -8,7 +8,7 @@ type ListNode struct {
 }
 
 func (l *ListNode) String() string {
-	s := ""
+	s := "List:"
 	for n := l; n != nil; n = n.Next {
 		s += fmt.Sprintf("%d", n.Val)
 	}
@@ -16,36 +16,43 @@ func (l *ListNode) String() string {
 }
 
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-	node := &ListNode{}
+	var previous *ListNode
+	var first *ListNode
 	lhs := l1
 	rhs := l2
 	carriage := 0
 	for {
-		if rhs == nil && lhs == nil {
+		if rhs == nil && lhs == nil && carriage == 0 {
 			break
 		}
 		lhsValue := 0
 		rhsValue := 0
 		if rhs != nil {
 			rhsValue = rhs.Val
+			rhs = rhs.Next
 		}
 		if lhs != nil {
 			lhsValue = lhs.Val
+			lhs = lhs.Next
 		}
-		node.Val = (rhsValue + lhsValue + carriage) % 10
-		carriage = (rhsValue + lhsValue + carriage) / 10
 
-		node = &ListNode{Next: node}
-		rhs = rhs.Next
-		lhs = lhs.Next
+		sum := (rhsValue + lhsValue + carriage) % 10
+		carriage = (rhsValue + lhsValue + carriage) / 10
+		node := &ListNode{Val: sum}
+		if previous != nil {
+			previous.Next = node
+		} else {
+			first = node
+		}
+		previous = node
 	}
-	return node
+	return first
 }
 
 func main() {
 	l1 := &ListNode{2, &ListNode{4, &ListNode{3, nil}}}
-	fmt.Println(l1)
 	l2 := &ListNode{5, &ListNode{6, &ListNode{4, nil}}}
-	fmt.Println(l2)
 	fmt.Println(addTwoNumbers(l1, l2))
+
+	fmt.Println(addTwoNumbers(&ListNode{5, nil}, &ListNode{5, nil}))
 }
